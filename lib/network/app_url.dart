6ffa303fbc149498abef8d5ce2a9d3342232e42e
dpsbhati <dps.microlentsystems@gmail.com>
2025-07-30 12:31:45 +0530

    remove unused code

--- lib/network/app_url.dart
+++ lib/network/app_url.dart
@@ -2,10 +2,6 @@
 // Whole App's API URL
 const baseUrl = 'https://luneta.microlent.com/api/'; // Base URL
 
-// const mapKey = 'AIzaSyB2JtqK5DXLKUGE1GCjH0TzX2xbHJPzFug'; // Base URL
-const mapKey = 'AIzaSyCuacSCdbor7YMMMO2lR4x3otzJkRcW2rE'; // Base URL
-
-
 const loginUrl = '${baseUrl}user/login';
 const signupUrl = '${baseUrl}user/register';
 const forgotUrl = '${baseUrl}Login/forget-password';
@@ -15,6 +11,4 @@ const getAllRank = '${baseUrl}ranks/getAll';
 const seafarerProfileBasicInfo = '${baseUrl}seafarer-profile/basic-info';
 const sendOtpUrl = '${baseUrl}user/send-otp';
 const verifyOtpUrl = '${baseUrl}user/verify-otp';
-
-
-
+const getSeafarerCompleteProfile = '${baseUrl}seafarer-profile/getCompleteProfileDataMobile/';
