c55830845e5b65ee1c18ba8579c82e35aae259aa
dpsbhati <dheerendrapratap@microlent.com>
2025-07-30 17:47:27 +0530

    commit

--- lib/network/app_url.dart
+++ lib/network/app_url.dart
@@ -6,9 +6,13 @@ const loginUrl = '${baseUrl}user/login';
 const signupUrl = '${baseUrl}user/register';
 const forgotUrl = '${baseUrl}Login/forget-password';
 const otpVerifyUrl = '${baseUrl}Login/verify-code';
-const resetUrl = '${baseUrl}Login/reset-password';
+const resetUrl = '${baseUrl}user/reset-password';
 const getAllRank = '${baseUrl}ranks/getAll';
 const seafarerProfileBasicInfo = '${baseUrl}seafarer-profile/basic-info';
-const sendOtpUrl = '${baseUrl}user/send-otp';
+const sendOtpUrl = '${baseUrl}user/forget-password-mobile';
 const verifyOtpUrl = '${baseUrl}user/verify-otp';
+const verifyOtpForgotUrl = '${baseUrl}user/verify-otp-mobile';
+
+
+
 const getSeafarerCompleteProfile = '${baseUrl}seafarer-profile/getCompleteProfileDataMobile/';
